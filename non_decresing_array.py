1cd03a975fc2355ebf02b6e297fe48df1d1e303c
Yosef64 <98908763+Yosef64@users.noreply.github.com>
2024-02-10 09:01:05 +0300

    Update non_decresing_array.py

--- non_decresing_array.py
+++ non_decresing_array.py
@@ -1,12 +1,14 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        nMistake = 0
+        nMistake ,tem = 0,[]
         for ind in range(len(nums)-1):
             if nums[ind] > nums[ind+1]:
                 if nMistake > 0:
                     return False
-                if ind > 0 and nums[ind-1] > nums[ind+1]:
+                if tem and tem[-1] > nums[ind+1]:
                     nums[ind+1] = nums[ind]
                 nMistake += 1
+            tem.append(nums[ind])
         return True
+        
         
