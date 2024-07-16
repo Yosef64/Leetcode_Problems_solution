0f1109b1433e17241df7e42837b9145e35cf402c
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-07-16 14:41:16 +0300

    Time: 931 ms (63.48%), Space: 49.8 MB (51.85%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,21 +1,27 @@
 class Solution:
-    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        routes = {(a,b) for a,b in connections}
-        dic = defaultdict(list)
-        changes , visited = 0 , set([0])
-        for a,b in connections:
-            dic[a].append(b)
-            dic[b].append(a)
-        def fn(route):
-            nonlocal changes
-            for neigbor in dic[route]:
-                if neigbor in visited:
-                    continue
-                if (neigbor,route) not in routes:
-                    changes += 1
-                
-                visited.add(neigbor)
-                fn(neigbor)
-            return 
-        fn(0)
-        return changes
+    def __init__(self):
+        self.count = 0
+
+    def dfs(self, node, parent, adj):
+        if node not in adj:
+            return
+        for nei in adj[node]:
+            neighbor = nei[0]
+            sign = nei[1]
+            if neighbor != parent:
+                self.count += sign
+                self.dfs(neighbor, node, adj)
+
+    def minReorder(self, n, connections):
+        adj = {}
+        for connection in connections:
+            if connection[0] not in adj:
+                adj[connection[0]] = []
+            adj[connection[0]].append([connection[1], 1])
+
+            if connection[1] not in adj:
+                adj[connection[1]] = []
+            adj[connection[1]].append([connection[0], 0])
+
+        self.dfs(0, -1, adj)
+        return self.count
