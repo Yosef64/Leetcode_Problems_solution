b954a7e86bee847820bb2b6d3a58f0e5fe4be26e
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-07-16 05:53:03 +0300

    Time: 1489 ms (43.27%), Space: 63.9 MB (89.41%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.py
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.py
@@ -3,22 +3,21 @@ class Solution:
 
         for ind, ls in enumerate(tasks):
             ls.append(ind)
-        
-        
+
         tasks.sort(key=lambda task: task[0])
-        n , heap =  len(tasks), []
-        cpuTime , i= tasks[0][0], 0
+        heap , n = [] , len(tasks)
+        i , cpuTime = 0,tasks[0][0]
         ans = []
         
         while heap or i < n:
             while i < n and cpuTime >= tasks[i][0]:
-                heapq.heappush(heap, (tasks[i][1], tasks[i][2])) 
+                heapq.heappush(heap,[tasks[i][1],tasks[i][2]])
                 i += 1
             
             if not heap:
                 cpuTime = tasks[i][0]
             else:
-                time, ind = heapq.heappop(heap)
+                time , ind = heapq.heappop(heap)
                 cpuTime += time
                 ans.append(ind)
 
