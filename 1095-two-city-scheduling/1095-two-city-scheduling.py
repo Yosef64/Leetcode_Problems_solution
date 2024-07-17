c1235fcc62fc1bf8ac773ce3f34822ced2225a06
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-07-17 12:40:39 +0300

    Time: 48 ms (30.39%), Space: 16.6 MB (19.09%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.py
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -1,17 +1,5 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        ans , n = 0 , len(costs)
-        dic = defaultdict(list)
-        for ind, (a , b) in enumerate(costs):
-            if a < b:
-                dic['a'].append(b-a)
-            else:
-                dic['b'].append(a-b)
-            ans += min(a,b)
-        
-        dif = len(dic['a']) - len(dic['b'])
-        if dif == 0:return ans
-        temp = sorted(dic['b']) if dif < 0 else sorted(dic['a'])
-        for ind in range(len(temp) - n//2):
-            ans += temp[ind]
-        return ans
+
+        costs, L = sorted(costs, key = lambda a: a[0] - a[1]), len(costs)//2
+        return sum(c[0] for c in costs[:L]) + sum(c[1] for c in costs[L:])
