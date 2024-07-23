fb5d9dd0c2a5e27f1c6bef2f53b75349175e8642
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-07-23 09:20:51 +0300

    Time: 349 ms (98.03%), Space: 21.9 MB (38.7%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.py
@@ -1,19 +1,15 @@
 class Solution:
-    def restoreMatrix(self, rowSum, colSum):
-        N = len(rowSum)
-        M = len(colSum)
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        r, c = len(rowSum), len(colSum)
+        arr = [[0]*c for _ in range(r)]
+        i, j = 0, 0
+        while i<r and j < c:
+            x = min(rowSum[i], colSum[j])
 
-        curr_row_sum = [0] * N
-        curr_col_sum = [0] * M
-
-        orig_matrix = [[0] * M for _ in range(N)]
-        for i in range(N):
-            for j in range(M):
-                orig_matrix[i][j] = min(
-                    rowSum[i] - curr_row_sum[i], colSum[j] - curr_col_sum[j]
-                )
-
-                curr_row_sum[i] += orig_matrix[i][j]
-                curr_col_sum[j] += orig_matrix[i][j]
-
-        return orig_matrix
+            arr[i][j] = x
+            rowSum[i] -= x
+            colSum[j] -= x
+            i += (rowSum[i] == 0) 
+            j += (colSum[j] == 0)
+            print(x,i,j)
+        return arr
