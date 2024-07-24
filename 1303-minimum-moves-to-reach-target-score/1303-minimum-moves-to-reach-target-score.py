ecdbc97b12e6181a1d26cb88d005e28c5cd09677
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-07-24 10:17:55 +0300

    Time: 39 ms (28.9%), Space: 16.5 MB (82.11%) - LeetHub

--- 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.py
+++ 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.py
@@ -3,11 +3,11 @@ class Solution:
         moves = 0
     
         while target > 1 and maxDoubles:
-            if target % 2 == 0 and maxDoubles > 0:
+            if target % 2 == 0:
                 target //= 2
                 maxDoubles -= 1
             else:
                 target -= 1
-            moves += 1
+            moves += 1 
         
         return moves + target - 1
