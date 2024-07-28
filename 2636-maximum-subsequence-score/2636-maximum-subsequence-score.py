85c11d311c4aca7e3bbf9616fc4d62920a7206ee
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-07-28 20:39:03 +0300

    Time: 960 ms (7.3%), Space: 49.8 MB (5.03%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
@@ -1,26 +1,23 @@
 import heapq
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        lst=list(zip(nums2,nums1))
+        lst=[[nums2[ind],nums1[ind]] for ind in range(len(nums1))]
         lst.sort(key=lambda x:(-x[0],-x[1]))
-        flst=[]
-        heapq.heapify(flst)
-        i=0
-        sm=0
-        ef=float("infinity")
-        prd=float("-infinity")
-        while i<k:
-            # x=lst.pop(0)
-            heapq.heappush(flst,lst[i][1])
+        heap = []
+        i , sm = 0 , 0
+        ef , prd = float("infinity") , float("-infinity")
+        while i < k:
+           
+            heapq.heappush(heap,lst[i][1])
             ef=min(ef,lst[i][0])
             sm+=lst[i][1]
             i+=1
         prd=max(prd,sm*ef)
         while i < len(nums1):
-            x=heapq.heappop(flst)
+            x=heapq.heappop(heap)
             sm-=x
             # y=lst.pop(0)
-            heapq.heappush(flst,lst[i][1])
+            heapq.heappush(heap,lst[i][1])
             ef = lst[i][0]
             sm+=lst[i][1]
             prd=max(prd,sm*ef)
