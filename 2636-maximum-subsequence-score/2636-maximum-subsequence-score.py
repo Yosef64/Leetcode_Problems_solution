3c87f27a9f469c4ed10acddb35aab4b2b99f2f45
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-07-29 20:17:43 +0300

    Time: 770 ms (46.41%), Space: 37 MB (82.8%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
@@ -1,25 +1,11 @@
-import heapq
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        lst=[[nums2[ind],nums1[ind]] for ind in range(len(nums1))]
-        lst.sort(key=lambda x:(-x[0],-x[1]))
-        heap = []
-        i , sm = 0 , 0
-        ef , prd = float("infinity") , float("-infinity")
-        while i < k:
-           
-            heapq.heappush(heap,lst[i][1])
-            ef=min(ef,lst[i][0])
-            sm+=lst[i][1]
-            i+=1
-        prd=max(prd,sm*ef)
-        while i < len(nums1):
-            x=heapq.heappop(heap)
-            sm-=x
-            # y=lst.pop(0)
-            heapq.heappush(heap,lst[i][1])
-            ef = lst[i][0]
-            sm+=lst[i][1]
-            prd=max(prd,sm*ef)
-            i += 1
-        return prd
+        res, prefixSum, minHeap = 0, 0, []
+
+        for a, b in sorted(list(zip(nums1, nums2)), key=itemgetter(1), reverse=True):
+            prefixSum += a
+            heappush(minHeap, a)
+            if len(minHeap) == k:
+                res = max(res, prefixSum * b)
+                prefixSum -= heappop(minHeap)                           
+        return res
