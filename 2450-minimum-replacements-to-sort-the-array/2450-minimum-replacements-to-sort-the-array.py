b2227fafc3fb3d5a7c3a304735def269de64864f
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-08-07 16:34:35 +0300

    Time: 416 ms (60.84%), Space: 30.4 MB (63.12%) - LeetHub

---
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        n = len(nums) - 1
+        Min , ans = nums[-1] , 0
+        for ind in range(n - 1,-1,-1):
+            if nums[ind] <= Min:
+                Min = nums[ind]
+            else:
+                ways = (nums[ind] + Min - 1) // Min  
+
+                ans += ways - 1
+                
+                Min = nums[ind] // ways
+
+        return ans
