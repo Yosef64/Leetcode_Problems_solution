211a009d81edee81db475f9a0ffb513b7a321c50
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-08-13 23:04:34 +0300

    Time: 1226 ms (91.97%), Space: 62.7 MB (62.89%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -4,10 +4,9 @@ class Solution:
         dp = [0] * (n)  
         dp[-1] = questions[-1][0]
         for ind in range(n-2,-1,-1):
-            jump = float("-inf")
-            con = questions[ind][1] + ind+1 < n
-            if con:
+            jump = 0
+            if questions[ind][1] + ind+1 < n:
                 jump = dp[questions[ind][1]+1+ind]
-            dp[ind] = max(jump+questions[ind][0],dp[ind+1]) if con else max(questions[ind][0],dp[ind+1])
+            dp[ind] = max(jump+questions[ind][0],dp[ind+1]) 
 
         return dp[0]
