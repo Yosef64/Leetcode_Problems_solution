837017d92481bdba61bc4504573c72c41bf5bdec
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-08-21 17:01:36 +0300

    Time: 2294 ms (31.08%), Space: 25.3 MB (5.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,12 +3,15 @@ class Solution:
         if len(s) <= 1:
             return s
         
-        Max_Len=1
-        Max_Str=s[0]
-        for i in range(len(s)-1):
-            for j in range(i+1,len(s)):
-                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
-                    Max_Len = j-i+1
-                    Max_Str = s[i:j+1]
-
+        Max_Len = 1
+        Max_Str = s[0]
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+            for j in range(i):
+                if s[j] == s[i] and (i-j <= 2 or dp[j+1][i-1]):
+                    dp[j][i] = True
+                    if i-j+1 > Max_Len:
+                        Max_Len = i-j+1
+                        Max_Str = s[j:i+1]
         return Max_Str
