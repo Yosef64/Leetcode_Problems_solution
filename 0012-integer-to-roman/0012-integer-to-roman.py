0aa515c359a7551188a703aa89c4f28e6b3a0336
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-08-28 23:14:36 +0300

    Time: 32 ms (98.7%), Space: 16.5 MB (55.69%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,6 +1,6 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        h = {
+        dic = {
             1:'I',
             5:'V',
             10:'X',
@@ -10,14 +10,12 @@ class Solution:
             1000:'M'
         }
         small , large,ans = 1,10,''
-        while num>0:
-            t = num%10
-            if t>=5:
-                ans=h[small]+h[large]+ans if t+1==10 else h[5*small]+h[small]*(t-5) + ans
-            else:
-                ans = h[small]+h[5*small] + ans if t+1==5 else h[small]*t+ans
-            large*=10
-            small*=10
-            num//=10
+        while num > 0:
+            t = num % 10
+            if t >= 5:
+                ans = dic[small] + dic[large] + ans if t + 1 == 10 else dic[5 * small] + dic[small] * (t-5) + ans
+            else:ans = dic[small] + dic[5 * small] + ans if t + 1 == 5 else dic[small] * t + ans
+            large , small = large * 10 , small * 10
+            num //= 10
         return ans
                 
