b03f7895e765f5ecd455785621b3753796ebb897
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-08-28 22:54:11 +0300

    Time: 44 ms (6.37%), Space: 16.4 MB (91.06%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -3,12 +3,9 @@ class Solution:
         dp = [0 for _ in range(n+1)]
         dp[1] = 1
         for num in range(2,n+1):
-            dp[num] = dp[num-1]
-            for i in range(1,num):
-                Max = max(dp[num-i]*dp[i-1],max(dp[num-i],dp[i-1]))
-                # print((num,i),Max)
-                dp[num] += Max
-            # print(num,dp[num])
+            dp[num] = dp[num-1]*2
+            for i in range(2,num):
+                dp[num] += dp[num-i]*dp[i-1]
         return dp[n]
 
 
