b476f5d207016116f16ca29e2c56bb6deafca786
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-08-29 08:55:01 +0300

    Time: 134 ms (6.73%), Space: 23.3 MB (5.93%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,7 +3,7 @@ class Solution:
         ans = []
         for i in range(n+1):
             count = 0
-            while i>0:
+            while i > 0:
                 if i & 1 == 1:
                     count += 1
                 i >>= 1
