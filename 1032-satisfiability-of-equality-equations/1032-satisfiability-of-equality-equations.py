4da148c67368d6e27e9e4604800c83a1e920c5de
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-09-15 11:55:56 +0300

    Time: 190 ms (5.61%), Space: 16.8 MB (48.22%) - LeetHub

---
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        graph = defaultdict(list)
+        for eq in equations:
+            if eq[1] != '!':
+                graph[eq[0]].append(eq[-1])
+                graph[eq[-1]].append(eq[0])
+        print(graph)
+        def dfs(node,path,t):
+            if node in path or t == node:
+                return not t == node
+            for child in graph[node]:
+                if not dfs(child,path.union({node}),t):
+                    return False
+            return True
+        for eq in equations:
+            if eq[1]=='!' and not (dfs(eq[-1],set(),eq[0]) and dfs(eq[0],set(),eq[-1])):
+                return False
+        return True
+                
