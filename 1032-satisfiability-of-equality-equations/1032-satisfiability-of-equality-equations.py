bf1463da259de8dc9dae3c17b77e49bfeb0655b3
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-09-16 08:45:10 +0300

    Time: 41 ms (87.89%), Space: 16.8 MB (48.21%) - LeetHub

--- 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.py
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.py
@@ -1,19 +1,21 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        graph = defaultdict(list)
+        parent = [ind for ind in range(26)]
+        def find(num):
+            if num == parent[num]:return num
+
+            return find(parent[num])
+        def union(num1,num2):
+            pNum1 , pNum2 = find(num1),find(num2)
+            parent[pNum1] = pNum2
+        
         for eq in equations:
-            if eq[1] != '!':
-                graph[eq[0]].append(eq[-1])
-                graph[eq[-1]].append(eq[0])
-        def dfs(node,path,t):
-            if node in path or t == node:
-                return not t == node
-            for child in graph[node]:
-                if not dfs(child,path.union({node}),t):
-                    return False
-            return True
+            if eq[1] == "=":
+                char1,char2 = ord(eq[0]) - 97 , ord(eq[-1]) - 97
+                union(char1,char2)
         for eq in equations:
-            if eq[1]=='!' and not (dfs(eq[-1],set(),eq[0]) and dfs(eq[0],set(),eq[-1])):
-                return False
+            if eq[1]=='!':
+                char1 , char2 = ord(eq[0]) - 97 , ord(eq[-1]) - 97
+                if find(char1) == find(char2):return False
         return True
                 
