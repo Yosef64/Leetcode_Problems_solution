a8d38a9d7f83f3d562f3b765e0cd66044d4802e8
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-09-16 14:19:12 +0300

    Time: 268 ms (5.04%), Space: 17.2 MB (85.9%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,20 +1,20 @@
 class Solution:
     def findCircleNum(self, c: List[List[int]]) -> int:
-        def dfs(node):
-            for neighbor in range(len(c[node])):
-                if c[node][neighbor]and neighbor not in seen:
-                    seen.add(neighbor)
-                    dfs(neighbor)
-        
-        n = len(c)
-
-        seen = set()
-        ans = 0
-        
-        for i in range(n):
-            if i not in seen:
-                ans += 1
-                seen.add(i)
-                dfs(i)
-        
-        return ans
+        rows,cols = len(c) , len(c[0])
+        nodes = [ind for ind in range(rows)]
+        def find(cur):
+            if cur == nodes[cur]:
+                return cur
+            return find(nodes[cur])
+        def union(n1, n2):
+            pn1 , pn2 = find(n1),find(n2)
+            nodes[pn1] = pn2
+        for row in range(rows):
+            for col in range(cols):
+                if c[row][col] == 1:
+                    union(row,col)
+        count = 0
+        for ind in range(len(nodes)):
+            if ind == nodes[ind]:
+                count += 1
+        return count
