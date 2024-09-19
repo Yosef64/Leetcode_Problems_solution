603eae38304d61af276d79d054a085ab4faf0089
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-09-19 20:40:24 +0300

    Time: 2576 ms (5%), Space: 622.5 MB (5.01%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @cache
+        def dp(buy,ind,count):
+            if ind == len(prices) or count > 2:
+                return 0
+            profit = 0
+            if buy:
+                profit = max(dp(False,ind+1,count) - prices[ind],dp(True,ind+1,count))
+            else:
+                profit = max(dp(True,ind+1,count+1)+prices[ind],dp(False,ind+1,count))
+            return profit
+        return dp(True,0,1)
+
