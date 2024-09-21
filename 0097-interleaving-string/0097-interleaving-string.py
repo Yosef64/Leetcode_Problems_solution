d68288c136191de173b354433882a06c047fc168
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-09-21 14:47:30 +0300

    Time: 41 ms (63.45%), Space: 18.2 MB (5.71%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,14 +1,15 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n , m , tn = len(s1),len(s2),len(s3)
         @cache
         def dp(f,s,t):
-            if t == len(s3):
-                return f == len(s1) and s == len(s2)
+            if t == tn:
+                return f == n and s == m
             
             fc , sc = False,False
-            if f < len(s1) and s3[t] == s1[f]:
+            if f < n and s3[t] == s1[f]:
                 fc = dp(f+1,s,t+1)
-            if s < len(s2) and s3[t] == s2[s]:
+            if s < m and s3[t] == s2[s]:
                 sc = dp(f,s+1,t + 1)
             return fc or sc
         return dp(0,0,0)
