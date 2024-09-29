ec0937269fab22eef75387e1023dfdb0e2b18848
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-09-29 22:26:08 +0300

    Time: 41 ms (35.66%), Space: 16.7 MB (37.59%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,25 +5,15 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        so=ListNode(0,next=None)
-        yo=so
-        if list1==None and list2:
-            return list2
-        elif list2==None and list1:
-            return list1  
-        while list1 and list2:
-            if list1.val>list2.val:
-                yo.next=list2
-                yo=yo.next
-                list2=list2.next
-                if list2==None:
-                    yo.next=list1
-            elif list1.val<=list2.val:
-                yo.next=list1
-                yo=yo.next
-                list1=list1.next
-                if list1==None:
-                    yo.next=list2
-                 
-        return so.next
-            
+        def reverseLinked(node1,node2):
+            if node1 == None:
+                return node2
+            if node2 ==None:
+                return node1
+            if node1.val<node2.val:
+                node1.next = reverseLinked(node1.next,node2)
+                return node1
+            else:
+                node2.next = reverseLinked(node1,node2.next)
+                return node2
+        return reverseLinked(list1,list2)
