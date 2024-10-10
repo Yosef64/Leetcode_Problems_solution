f027c17e50efba13e034c29f59bc1eb620c3e380
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-10-10 21:31:35 +0300

    Time: 1036 ms (71.28%), Space: 38 MB (5.17%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,7 +1,21 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        ans = []
-        heapify(nums)
-        while nums:
-            ans.append(heapq.heappop(nums))
-        return ans
+        def fn(st,end):
+            if st == end:
+                return [nums[st]]
+            
+            left = fn(st,(end - st)//2 + st)
+            right = fn((end - st)//2 + st + 1,end)
+            arr = []
+            f ,s = 0 , 0
+            while f < len(left) and s < len(right):
+                if left[f] < right[s]:
+                    arr.append(left[f])
+                    f += 1
+                else:
+                    arr.append(right[s])
+                    s += 1
+            arr.extend(left[f:])
+            arr.extend(right[s:])
+            return arr
+        return fn(0,len(nums) - 1)
