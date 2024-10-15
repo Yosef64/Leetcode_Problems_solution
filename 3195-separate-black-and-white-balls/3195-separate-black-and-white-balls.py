69b2481ce72f518c13439e071d1e555c0e1bbda3
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-10-15 15:43:27 +0300

    Time: 205 ms (5.97%), Space: 18.4 MB (15.82%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -2,7 +2,7 @@ class Solution:
     def minimumSteps(self, s: str) -> int:
         ans , s = 0 , list(s)
         black , white = 0 , 1
-        while black <= white and white < len(s):
+        while black < white and white < len(s):
             if s[black] == "1" and s[white] == "0":
                 ans += white - black
                 s[black] , s[white] = s[white],s[black]
@@ -10,7 +10,5 @@ class Solution:
                 black += 1
             
             white+= 1  
-
-            # if black == white:
-            #     white += 1        
+        
         return ans
