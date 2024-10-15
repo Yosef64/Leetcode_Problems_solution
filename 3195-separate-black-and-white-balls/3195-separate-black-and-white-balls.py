7cdcd020ed9849975b09d5608dbf92e9ce25fe53
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-10-15 15:42:01 +0300

    Time: 201 ms (6.27%), Space: 18.5 MB (15.82%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        ans , s = 0 , list(s)
+        black , white = 0 , 1
+        while black <= white and white < len(s):
+            if s[black] == "1" and s[white] == "0":
+                ans += white - black
+                s[black] , s[white] = s[white],s[black]
+            if s[black] == "0":
+                black += 1
+            
+            white+= 1  
+
+            # if black == white:
+            #     white += 1        
+        return ans
