987a95362ccc329708bfc06b2463fab1d883b777
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-11-11 21:20:03 +0300

    Time: 1211 ms (5.04%), Space: 412.6 MB (5%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        row = [0 for _ in range(amount+1)]
-        row[0] = 1
-        for coin in coins:
-            for amt in range(coin,amount+1):
-                row[amt] += row[amt-coin]
-        return row[-1]
-
+        @cache
+        def dp(ind,amt):
+            if ind == len(coins) or amt <= 0:
+                return int(amt == 0)
+            
+            return dp(ind,amt-coins[ind])+dp(ind+1,amt)
+        return dp(0,amount)
             
