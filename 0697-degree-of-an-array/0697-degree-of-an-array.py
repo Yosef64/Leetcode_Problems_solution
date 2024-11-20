66c48dff565489c883ea1081b1005660fbcad239
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-11-20 22:20:49 +0300

    Time: 19 ms (69.51%), Space: 19 MB (21.7%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -8,11 +8,8 @@ class Solution:
                 count[val] = [count[val][0]+1,count[val][1],ind]
         minLen , minVal = count[nums[0]][-1] - count[nums[0]][1]+1,nums[0]
         for key in count:
-            if count[key][0] > count[minVal][0]:
+            if count[key][0] > count[minVal][0] or (count[key][0] == count[minVal][0] and count[key][-1] - count[key][1] + 1 < minLen):
                 minVal = key
                 minLen = count[key][-1] - count[key][1] + 1
-            elif count[key][0] == count[minVal][0]:
-                if count[key][-1] - count[key][1] + 1 < minLen:
-                    minVal = key
-                    minLen = count[key][-1] - count[key][1] + 1
+           
         return minLen
