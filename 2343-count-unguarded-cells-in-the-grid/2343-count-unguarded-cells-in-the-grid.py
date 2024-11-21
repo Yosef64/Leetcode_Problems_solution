6be396efd944db9fd874e7d70b328d7651980965
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-11-21 07:00:48 +0300

    Time: 347 ms (72%), Space: 40.2 MB (66.67%) - LeetHub

---
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        walls = {(x,y) for x,y in walls}
+        g = [[0 for col in range(n)] for row in range(m)]
+        for x, y in guards:  
+            g[x][y] = 2    
+        for x, y in walls:
+            g[x][y] = 2
+        
+        dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        for gx, gy in guards:
+            for dx, dy in dirs: 
+                x, y = gx, gy
+                while True:
+                    x += dx
+                    y += dy
+                    if x < 0 or x >= m or y < 0 or y >= n or g[x][y] == 2:
+                        break
+                    g[x][y] = 1
+        count = sum(col == 0 for row in g for col in row)
+        return count
