1e1d11b4b873d2baf292489190036819f84be642
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-12-04 21:59:01 +0300

    Time: 115 ms (6.67%), Space: 21.8 MB (5.32%) - LeetHub

---
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        presum , n = [nums[0]], len(nums)
+        for ind in range(1,n):
+            presum.append(presum[-1]+nums[ind])
+        print(presum)
+        dic ,res = defaultdict(int), 0
+        l = 0
+        for r in range(n):
+            if (presum[r] == goal and presum[r] + presum[l] == goal) and (presum[r]!=0 or presum[l] !=0):
+                dic[presum[r]] += 1
+            while l < r and presum[r]-presum[l] >= goal:
+                print(goal,r)
+                if presum[r] - presum[l] == goal:
+                    dic[presum[r]] += 1
+                l += 1
+            if presum[r] == goal and not dic[presum[r]]:dic[presum[r]] += 1
+            res += dic[presum[r]]
+        return res
+        
