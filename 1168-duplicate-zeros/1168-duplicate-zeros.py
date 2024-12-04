bb48dbfe4bfaf81dfd56cc5d7116060ed20f3546
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-12-04 11:49:43 +0300

    Time: 1 ms (77.23%), Space: 17.5 MB (9.76%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.py
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -3,18 +3,15 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
-        countZero , n = 0 , len(arr)
-        for ele in arr:
-            if ele == 0:countZero+=1
-        
+        countZero , n = arr.count(0) , len(arr)
         tempArr = [0]*(n+countZero)
         f,s = 0, 0
         while f < n:
             tempArr[s] = arr[f]
             if arr[f] == 0:
                 s += 1
-            s += 1
-            f += 1
+            s ,f= s + 1,f + 1
+            
         for ind in range(n):
             arr[ind] = tempArr[ind]
             
