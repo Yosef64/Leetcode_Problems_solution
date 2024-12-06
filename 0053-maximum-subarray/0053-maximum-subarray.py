f5e40ddcbbf3a3246eab3c8c853e6f4cd60a5da5
Yoseph Alemu <98908763+Yosef64@users.noreply.github.com>
2024-12-06 22:22:15 +0300

    Time: 95 ms (19.04%), Space: 31.2 MB (62.94%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        presum , minEle = nums[0] , nums[0]
-        ans = nums[0]
+        cMax = gMax = nums[0]
         for num in nums[1:]:
-            presum += num
-            ans = max(ans,presum,presum - minEle,presum - num)
-            minEle = min(minEle,presum)
-        return ans 
+            cMax = max(num,cMax+num)
+            gMax = max(gMax,cMax)
+        return gMax
